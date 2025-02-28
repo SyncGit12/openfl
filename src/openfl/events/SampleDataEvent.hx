4ae665797ee950e729abe0b984ed8e305c9bc84b
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-02-28 13:47:55 -0300

    it was this all along

--- src/openfl/events/SampleDataEvent.hx
+++ src/openfl/events/SampleDataEvent.hx
@@ -177,20 +177,6 @@ class SampleDataEvent extends Event
 	}
 
 	#if lime_openal
-	private function getBufferSize():Int
-	{
-		var bufferSize:Int = Std.int(data.length / 4 / 2);
-		if (bufferSize >= 2048 && bufferSize <= 8192)
-		{
-			return bufferSize;
-		}
-		else
-		{
-			throw new Error("To be consistent with flash the listener function registered to SampleDataEvent has to provide between 2048 and 8192 samples.");
-		}
-		return bufferSize;
-	}
-
 	private function getSamples(outputBuffer:ByteArray):Void
 	{
 		var bytesLength:Int = data.length;
